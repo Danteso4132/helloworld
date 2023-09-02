a4c6b426f9b6af40284ff1497ae981acc48d2fb1
Danteso4132 <karelinilya1999@gmail.com>
2023-09-02 10:27:22 +0300

    Initial commit

---
+++ src/main/java/com/ilka/helloworld/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.ilka.helloworld.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello world";
+    }
+}
